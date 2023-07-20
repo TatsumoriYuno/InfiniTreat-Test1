5595a7fdf19b3e83e3c938af71b50ec917c517c6
TatsumoriYuno <121322302+TatsumoriYuno@users.noreply.github.com>
2023-07-20 00:13:58 -0400

    Add files via upload

---
+++ InfiniTreat_Test/root/nested/TheScript.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import cv2
+import mss
+import numpy
+import keyboard
+import pyautogui
+from time import time, sleep, perf_counter
+import pause
+
+#Enter Now Postition
+#pyautogui.moveTo(x=900, y=152)
+#Chest Position
+#pyautogui.moveTo(x=25, y=842)
+#Finish Position
+##pyautogui(x=803, y=789)
+#Burger
+#pyautogui(x=700, y=712)
+
+def precise_delay(delay_amount):
+    prv_t = perf_counter()
+    
+    while round((perf_counter() - prv_t) * 1000) / 1000 - 0.001 <= delay_amount:
+        pass
+
+print("Press 'z' to start program.")
+print("Once started press 'q' to quit.")
+keyboard.wait('z')
+pyautogui.press('x')
+
+
+sct = mss.mss()
+zero_txt = cv2.imread("TERNOW.PNG")
+
+dimensions_counter = {'left': 850, 'top': 25, 'width': 250, 'height': 250}
+#dimensions_counter = {'left': 1100, 'top': 25, 'width': 250, 'height': 250}
+
+
+
+while True:
+    scr = numpy.array(sct.grab(dimensions_counter))
+    scr_remove = scr[:,:,:3]
+
+    result = cv2.matchTemplate(scr_remove, zero_txt, cv2.TM_CCOEFF_NORMED)
+    
+    _, max_val, _, max_loc = cv2.minMaxLoc(result)
+    #src = scr.copy()
+    if max_val > .79:
+        pyautogui.moveTo(x=900, y=152)
+        pyautogui.click(button='left')
+        precise_delay(.5)
+        pyautogui.press('i')
+        precise_delay(1.2)
+        pyautogui.moveTo(x=803, y=789)
+        pyautogui.click(button='left')
+        precise_delay(.5)
+        pyautogui.moveTo(x=700, y=712)
+        pyautogui.click(button='left')
+        precise_delay(7.35)
+        pyautogui.moveTo(x=1221, y=792)
+        pyautogui.click(button='left')
+        precise_delay(3)
+        pyautogui.keyDown('s')
+        precise_delay(6.4)
+        pyautogui.keyUp('s')
+        pyautogui.press('x')
+        max_val = 0
+        
+    cv2.imshow('P101 Timer', scr)
+    cv2.waitKey(1)
+    
+    if keyboard.is_pressed('q'):
+        break
+print("Program closed with 'q' key.")
